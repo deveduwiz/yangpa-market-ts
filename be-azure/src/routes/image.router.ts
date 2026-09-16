fd38c8fec2109ba23f4a855a6ee0815111c3379d
wizard113 <yeong.rok.lee@gmail.com>
2026-09-16 17:51:38 +0900

    befor front

--- be-azure/src/routes/image.router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import * as imageController from '../controllers/image.controller.js';
-
-const router = Router();
-
-router.get('/:filename', imageController.getImage);
-
-export default router;
